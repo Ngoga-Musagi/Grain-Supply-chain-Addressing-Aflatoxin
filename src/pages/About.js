e6a14564a2d793f492cfe605182ab6a9280a9d0c
Ngoga-Musagi <r.alexisngoga20@gmail.com>
2022-12-30 11:00:32 +0200

    done

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About =()=>{
+    return(
+        <div style={{marginTop:"150px"}}>
+            <h2>
+                {" "}
+                This is Grain Farming Management System
+                With React JS and node.Js with Express JS
+            </h2>
+        </div>
+    )
+}
+export default About;
